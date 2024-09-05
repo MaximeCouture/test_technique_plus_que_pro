a30144f465d1e0f1f75354b8f4549c87e6f2dbdb
lassus <maximecouture1@gmail.com>
2024-09-05 10:45:19 +0200

    add EasyAdmin conf for Movie & Genre

--- symfony-docker-main/src/Controller/Admin/MovieCrudController.php
+++ symfony-docker-main/src/Controller/Admin/MovieCrudController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Genre;
 use App\Entity\Movie;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +20,24 @@ class MovieCrudController extends AbstractCrudController
         return Movie::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDateFormat('d/m/Y')
+            ->setDateTimeFormat('d/m/Y');
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('title')->setRequired(true),
+            TextField::new('tagline')->onlyOnForms(),
+            TextEditorField::new('overview'),
+            DateTimeField::new('releaseDate')->setRequired(true),
+            AssociationField::new('genres')->setCrudController(GenreCrudController::class),
         ];
     }
-    */
+
 }
--- symfony-docker-main/src/Entity/Genre.php
+++ symfony-docker-main/src/Entity/Genre.php
@@ -41,4 +41,9 @@ class Genre
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
 }
